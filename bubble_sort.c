ed6e4064d5db4e54bb1d2e29833653baeb28d495
Chethan L <chethan.shantha@gmail.com>
2022-02-28 17:39:07 +0000

    modified bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main(){
-    int arr[100],nElem;
+void main()
+{
+    int arr[100], nElem;
     printf("Enter the number of ellements.");
-    scanf("%d",&nElem);
+    scanf("%d", &nElem);
     printf("Enter the array elements : ");
-    for(int i = 0;i<nElem;i++)
-        scanf("%d",&arr[i]);
-    
+    for (int i = 0; i < nElem; i++)
+        scanf("%d", &arr[i]);
     int temp;
-		for(int j = nElem-1;j>1;j--)
-			for(int i = 0;i<j;i++)
-				if(arr[i]>arr[i+1])
-				{
-					temp = arr[i+1];
-					arr[i+1] = arr[i];
-					arr[i] = temp;
-				}
+    for (int j = nElem - 1; j > 1; j--)
+        for (int i = 0; i < j; i++)
+            if (arr[i] > arr[i + 1])
+            {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
     printf("After sorting\n");
-    for(int i = 0;i <nElem;i++)
-        printf("%d\t",arr[i]);
+    for (int i = 0; i < nElem; i++)
+        printf("%d\t", arr[i]);
 }
